877cb548fb117d4a3b975202e156590983d7c885
James Power <heathen79@gmail.com>
2015-12-28 20:19:51 -0500

    Excuses!!

---
+++ source/components/Excuse.react.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var style = {
+    fontSize: '120px',
+    fontWeight: '300',
+    display: 'inline-block',
+    margin: '20px 10px'
+};
+
+var Excuse = React.createClass({
+    render: function() {
+        return (
+            <h2 style={style}>{this.props.text}</h2>
+        );
+    }
+});
+
+module.exports = Excuse;
